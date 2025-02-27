7847999d8e82bacce88c500d9a12eba74379bf48
Adam Whitehouse <adswhitehouse95@gmail.com>
2025-02-27 20:46:07 +0000

    added animation

---
+++ script.js
@@ -0,0 +1,11 @@
+let submitBtn = document.querySelector(".jsSubmit")
+let popup = document.querySelector(".jsPopup")
+let closePopupBtn = document.querySelector(".jsClosePopupBtn")
+
+submitBtn.addEventListener("click", () => {
+  popup.classList.add("animation")
+})
+
+closePopupBtn.addEventListener("click", () => {
+  popup.classList.remove("animation")
+})
